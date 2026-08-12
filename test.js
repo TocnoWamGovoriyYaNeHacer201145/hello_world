9cd3af4d33f0a5a280edfb623aad9e7e326f9e70
TocnoWamGovoriyYaNeHacer201145 <hacergames861@gmail.com>
2026-08-12 20:02:00 +0300

    Fixed test.js

--- test.js
+++ test.js
@@ -1,8 +1,1 @@
-const fs = require('fs');
-const vm = require('vm');
-
-function openContentSafe(file) {
-    const content = fs.readFileSync(file, 'utf8');
-    const context = vm.createContext({});
-    vm.runInContext(content, context);
-}
+console.log("Hello, world!");
